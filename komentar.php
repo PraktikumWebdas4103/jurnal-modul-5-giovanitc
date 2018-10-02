653a9843dca2b66c4814794a4fb74e70a084a445
giovanitc <43519327+giovanitc@users.noreply.github.com>
2018-10-02 16:37:05 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,19 @@
+<form action=" " method="POST">
+	<center>
+	<table>
+<tr>
+	<td>Komentar<input type="textbox" name="komentar" style="width: 100%; height: 50%; color: black; background-color: skyblue; border-radius: 5px"><br/></td>
+</tr>
+<tr>
+	<td><input type="Submit" name="submit" value="hitung"></td>
+</tr>
+	</table></center>
+</form>
 <?php
+if(isset($_POST['submit'])){
+	$komentar = $_POST['komentar'];
+	$hasil = str_word_count($komentar);
 
+	echo "<center>jumlah kata : $hasil</center>";
+}
 ?>
