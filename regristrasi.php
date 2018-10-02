ed70263314efc35ee76805e85664353966f9f454
giovanitc <43519327+giovanitc@users.noreply.github.com>
2018-10-02 16:36:00 +0700

    Update regristrasi.php

--- regristrasi.php
+++ regristrasi.php
@@ -4,7 +4,47 @@
 	<title>Form Registrasi</title>
 </head>
 <body>
+<form action=" " method="POST">
+	<center>
+	<table>
+<tr>
+	<td>Nim 	</td><td><input type="text" maxlength="10" name="nim"><br/></td>
+</tr>
+<tr>
+	<td>Nama	</td><td><input type="text" maxlength = "25" name="nama"><br/></td>
+</tr>
+<tr>
+	<td>Email	</td><td><input type="email" name="email"><br/></td>
+</tr>
+<tr>
+	<td><input type="Submit" name="submit" value="Submit"></td>
+</tr>
+	</table></center>
+</form>
+	<?php
+ 	$servername = "localhost";
+ 	$nim = "root";
+ 	$nama = "";
+ 	$db = "pendaftaran";
+ 	$con = new mysqli($servername, $nim, $nama, $db);
 
+ 	if($con==false){
+ 		die("Koneksi Gagal" . $con->connect_error);
+ 	
+ 	}
+ 	if(isset($_POST['submit'])){
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
+	$email = $_POST['email'];
+	$sql = "INSERT INTO mahasiswa (nim,nama,email) values ('$nim','$nama','$email')";
+	if (mysqli_query($con, $sql)){
+		echo "<center>Database sudah masuk</center>";
+	}else{
+		echo "ERROR";
+	}
+	}
+
+	?>
 
 
 </body>
